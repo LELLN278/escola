4986cb53c737daf55241a8941920f587c85c616c
LELLN <elmanoluiz27867@gmail.com>
2025-06-12 12:17:11 -0300

    CRUD de alunos

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const alunoController = require('../controllers/AlunoController');
+const loginRequired = require('../middlewares/loginRequired')
+
 const router = new Router();
 
 
 router.get('/', alunoController.index);
+router.get('/:id', alunoController.show);
+router.post('/', loginRequired, alunoController.store);
+router.put('/:id', loginRequired, alunoController.update);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 module.exports = router;
