635829b55880895818ec348249420f8f266e693f
LELLN <elmanoluiz27867@gmail.com>
2025-06-13 12:00:52 -0300

    Tratamento de Upload

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,14 +1,10 @@
 const { Router } = require('express');
-const multer = require('multer')
 
 const photoController = require('../controllers/PhotoController');
-const multerConfig = require('../config/multer');
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
 
-router.post('/', upload.single('photo'),  photoController.store);
+router.post('/', photoController.store);
 
 module.exports = router;
